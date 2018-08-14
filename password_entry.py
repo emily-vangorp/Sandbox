57e819c8aa005fec5e5495c3108e0370cd3467c3
emily-vangorp <emily.vangorp@my.jcu.edu.au>
2018-08-14 15:08:06 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,8 @@
 """Emily Van Gorp"""
+MIN_LENGTH = 6
+password = input("Password: ")
+while len(password) < MIN_LENGTH:
+    print("Too short.")
+    password = input("Password: ")
+for char in password:
+    print('*', end='')
